2a4736950b3b8fce9de22373c900e6b6f23ff25a
jnf <jeremy.flores@gmail.com>
2015-06-03 22:36:30 -0700

    Use the kitty decorator to dynamically generate a list of my cats plus a detail page for each cat.

--- my-site.rb
+++ my-site.rb
@@ -1,10 +1,19 @@
 require "sinatra"
 require "sinatra/reloader"
+require './lib/kitties'
 
 class MySite < Sinatra::Base
   register Sinatra::Reloader
 
   get "/" do
-    send_file "static/dayspa.html"
+    @kitties = Kitties::Kitty.all_the_cats
+    erb :index
+  end
+
+  get "/:name" do
+    @kitty  = Kitties::Kitty.new(params[:name])
+    @title  = "Nice to meet you, #{ @kitty.name }"
+
+    erb :kitty
   end
 end
